906d1d83b673a5b6de6ee80e7ac1e611b1c9d041
Ellie Burton <116921993+ellie-burton@users.noreply.github.com>
2024-01-25 14:27:48 -0600

    updates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentIndex = 0;
-const items = document.querySelector('.carousel');
+const items = document.querySelector('.carousel > div');
 const totalItems = items.children.length;
 
 function nextSlide() {
@@ -20,3 +20,15 @@ function updateCarousel() {
     const translateValue = -currentIndex * (150 + 10); // Adjust the width and margin of book covers
     items.style.transform = `translateX(${translateValue}px)`;
 }
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#myCarousel").carousel("prev");
+});
